e6b842f6465e278f537eaed44e02a3673b63fb77
Mahdi <mahdipakravan79@gmail.com>
2023-03-28 17:12:03 +0430

    feat : text animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { memo } from "react";
 import "./Home.css";
 import { Code } from "../components/code";
 import { buildClassNames } from "../utils/css";
 import { useOnMountAnimation } from "../utils/hooks/useOnMountAnimation";
-import { useAudio } from "../hooks/useAudio";
-import { AudioType } from "../constants/types";
-import { TRACK_MUSIC_1 } from "../constants/tracks";
+import AnimatedText from "react-animated-text-content";
 
 const md = `
 //welcome to my portfolio website !
@@ -17,53 +15,94 @@ const github = 'https://github.com/mahdipakravan'
 let mountedBefore = false;
 
 function Home() {
-  const frame1 = useOnMountAnimation({
-    delayTime: 1000,
-    showAnimation: !mountedBefore,
-  });
-  const frame2 = useOnMountAnimation({
-    delayTime: 4000,
-    showAnimation: !mountedBefore,
-  });
   const frame3 = useOnMountAnimation({
-    delayTime: 6500,
-    showAnimation: !mountedBefore,
+    delayTime: 3000,
     onAnimationEnd: () => (mountedBefore = true),
   });
-  // useAudio({
-  //   playSrc: TRACK_MUSIC_1,
-  //   audioType: AudioType.Music,
-  // });
   return (
     <div
       className={
         "h-full text-secondary-300 px-2 md:px-0 flex flex-col justify-center items-start"
       }
     >
-      <span
-        className={buildClassNames(
-          "font-regular text-sm text-secondary-50",
-          frame1
-        )}
-      >
-        Hello everyone , I'm ...
-      </span>
-
-      <span className={buildClassNames("font-semibold text-4xl mt-2", frame2)}>
-        Mahdi Pakravan.
-      </span>
-      <span
-        className={buildClassNames(
-          "font-regular text-xl text-secondary-200 mt-2 mb-20",
-          frame2
-        )}
-      >
-        {">"} Frontend Engineer :)
-      </span>
-
+      <Introduce />
       <Code className={buildClassNames("mb-20", frame3)} markdown={md} />
     </div>
   );
 }
 
+const Introduce = memo(
+  () => {
+    return (
+      <>
+        <span
+          className={buildClassNames("font-regular text-sm text-secondary-50")}
+        >
+          <AnimatedText
+            type="words" // animate words or chars
+            animation={{
+              x: "600px",
+              y: "-20px",
+              scale: 1.1,
+              ease: "ease-in-out",
+            }}
+            animationType="float"
+            duration={1}
+            tag="p"
+            className="animated-paragraph"
+            includeWhiteSpaces
+            threshold={1}
+            rootMargin="20%"
+          >
+            Hello everyone , I'm ...
+          </AnimatedText>
+        </span>
+        <span className={buildClassNames("font-semibold text-4xl mt-2")}>
+          <AnimatedText
+            type="words" // animate words or chars
+            animation={{
+              x: "200px",
+              y: "-20px",
+              scale: 1.1,
+              ease: "ease-in-out",
+            }}
+            animationType="wave"
+            duration={2}
+            tag="p"
+            className="animated-paragraph"
+            includeWhiteSpaces
+            threshold={0.6}
+            rootMargin="20%"
+          >
+            Mahdi Pakravan.
+          </AnimatedText>
+        </span>
+        <span
+          className={buildClassNames(
+            "font-regular text-xl text-secondary-200 mt-2 mb-20"
+          )}
+        >
+          <AnimatedText
+            type="words" // animate words or chars
+            animation={{
+              x: "400px",
+              y: "-20px",
+              scale: 1.6,
+              ease: "ease-in-out",
+            }}
+            animationType="float"
+            duration={4}
+            tag="span"
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="20%"
+          >
+            {"> Frontend Engineer :)"}
+          </AnimatedText>
+        </span>
+      </>
+    );
+  },
+  () => true
+);
 export default Home;
