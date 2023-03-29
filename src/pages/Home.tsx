80727dd6c58b95c6d425cf99e4022fc50a222317
Mahdi <mahdipakravan79@gmail.com>
2023-03-29 14:30:54 +0430

    feat : animation typist

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import { Code } from "../components/code";
 import { buildClassNames } from "../utils/css";
 import { useOnMountAnimation } from "../utils/hooks/useOnMountAnimation";
-import AnimatedText from "react-animated-text-content";
+import Typist from "react-typist";
 
 const md = `
 //welcome to my portfolio website !
@@ -16,7 +16,7 @@ let mountedBefore = false;
 
 function Home() {
   const frame3 = useOnMountAnimation({
-    delayTime: 3000,
+    delayTime: 5000,
     onAnimationEnd: () => (mountedBefore = true),
   });
   return (
@@ -26,6 +26,7 @@ function Home() {
       }
     >
       <Introduce />
+
       <Code className={buildClassNames("mb-20", frame3)} markdown={md} />
     </div>
   );
@@ -35,70 +36,28 @@ const Introduce = memo(
   () => {
     return (
       <>
-        <span
-          className={buildClassNames("font-regular text-sm text-secondary-50")}
-        >
-          <AnimatedText
-            type="words" // animate words or chars
-            animation={{
-              x: "600px",
-              y: "-20px",
-              scale: 1.1,
-              ease: "ease-in-out",
-            }}
-            animationType="float"
-            duration={1}
-            tag="p"
-            className="animated-paragraph"
-            includeWhiteSpaces
-            threshold={1}
-            rootMargin="20%"
+        <Typist cursor={{ hideWhenDone: true }}>
+          <span
+            className={buildClassNames(
+              "font-regular text-sm text-secondary-50"
+            )}
           >
             Hello everyone , I'm ...
-          </AnimatedText>
-        </span>
+          </span>
+        </Typist>
         <span className={buildClassNames("font-semibold text-4xl mt-2")}>
-          <AnimatedText
-            type="words" // animate words or chars
-            animation={{
-              x: "200px",
-              y: "-20px",
-              scale: 1.1,
-              ease: "ease-in-out",
-            }}
-            animationType="wave"
-            duration={2}
-            tag="p"
-            className="animated-paragraph"
-            includeWhiteSpaces
-            threshold={0.6}
-            rootMargin="20%"
-          >
+          <Typist startDelay={2000} cursor={{ show: false }}>
             Mahdi Pakravan.
-          </AnimatedText>
+          </Typist>
         </span>
         <span
           className={buildClassNames(
             "font-regular text-xl text-secondary-200 mt-2 mb-20"
           )}
         >
-          <AnimatedText
-            type="words" // animate words or chars
-            animation={{
-              x: "400px",
-              y: "-20px",
-              scale: 1.6,
-              ease: "ease-in-out",
-            }}
-            animationType="float"
-            duration={4}
-            tag="span"
-            includeWhiteSpaces
-            threshold={0.1}
-            rootMargin="20%"
-          >
-            {"> Frontend Engineer :)"}
-          </AnimatedText>
+          <Typist startDelay={3000} cursor={{ show: false }}>
+            {"> Frontend Engineer"}
+          </Typist>
         </span>
       </>
     );
